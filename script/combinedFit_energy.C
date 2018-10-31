0e3885ff8e52917af045918274258865fd69a7d5
Hsiao-Ru Pan <b0pan@pdsf7.nersc.gov>
2018-10-30 22:01:50 -0700

    Remove MINUIT1 related functions Fixed various bugs

--- script/combinedFit_energy.C
+++ script/combinedFit_energy.C
@@ -11,7 +11,7 @@ const double tau_he8 = 171.68;
 const double tau_b12 = 29.142;
 const double tau_b12_err = 0.0288;
 
-double fitMin = 5;
+double fitMin = 1.5;
 double fitMax = 5000;
 
 const double simplex_args[2] = {1000000, 0.1};
@@ -21,7 +21,7 @@ const double minos_args[1] = {1000000};
 const bool withB12 = true;
 
 const int n_muon_rates = 3 * n_range;
-const int e_slices = 10;
+const int e_slices = 20;
 const int n_slice = e_slices + 1;
 
 const int npars = 3 * n_range + 3 * e_slices + 1 + 1;
@@ -45,17 +45,6 @@ const int npars_single = 8;
 //[5]:ratio_lihe
 //[6]:n_b12
 //[7]:t_b12
-/*
-double _f_single(double *x, double *par){
-	double _x = x[0];
-	double f_ibd = par[1] * par[0] * exp(- par[0] * _x);
-	double f_lihe = par[2] * (par[5] * (par[0] + 1 / par[3]) * exp(-(par[0] + 1 / par[3]) * _x) + ( 1 - par[5] ) * (par[0] + 1 / par[4]) * exp(-(par[0] + 1 / par[4]) * _x) );
-	double f_b12 = par[6] * (par[0] + 2 / par[7]) * exp(-(par[0] + 2 / par[7]) * _x );
-
-	double sum = f_ibd + ( f_lihe + f_b12 );
-	return sum;
-}
-*/
 const int n_pdfs = 3;
 const char *_pdfs[n_pdfs] = {
 	"[1] * [0] * exp(-[0] * x )",
@@ -95,8 +84,7 @@ void parTrans(const double *par, double _par[n_range][n_slice][npars_single]){
 		1 //n_tot
 		1 //lihe ratio
 */
-	int bins = h[0][0]->GetNbinsX();
-	double scale = 5000 / bins;
+	double scale = h[0][0]->GetBinWidth(1);
 
 	double r_mu[n_range];
 	double n_lihe[n_range];
@@ -111,8 +99,8 @@ void parTrans(const double *par, double _par[n_range][n_slice][npars_single]){
 	int p_idx = 0;
 	for(int i=0;i<n_range;++i){
 		r_mu[i] = par[p_idx]; p_idx++;
-		n_lihe[i] = fabs(par[p_idx]) * scale; p_idx++;
-		n_b12[i] = fabs(par[p_idx]) * scale; p_idx++;
+		n_lihe[i] = par[p_idx] * scale; p_idx++;
+		n_b12[i] = par[p_idx] * scale; p_idx++;
 	}
 
 	for(int i=0;i<e_slices;++i){
@@ -213,9 +201,15 @@ void plotHists(int site, TH1 *h[n_range][n_slice], TF1 *f[n_range][n_slice], dou
 			c1->SaveAs(buf);
 		}
 	}
+
 }
 
-void minimizerParInit(int site, ROOT::Math::Minimizer *minim){
+void initialize_minimizer(int site, ROOT::Math::Minimizer *minim){
+	minim->SetErrorDef(0.5);
+	minim->SetTolerance(0.01);
+	minim->SetPrintLevel(2);
+	minim->SetStrategy(2);
+	minim->SetMaxFunctionCalls(1000000);
 /*
 		n_range //muon rates  
 		n_range //n_lihe
@@ -252,12 +246,11 @@ void minimizerParInit(int site, ROOT::Math::Minimizer *minim){
 
 		sprintf(buf, "n_b12_%d", i);
 		sprintf(par_names[p_idx], "%s", buf);
-		if(withB12){
+		if(withB12)
 			minim->SetLowerLimitedVariable(p_idx, buf, n_b12_init, n_b12_init * step_ratio, 0);
-		}else{
-			minim->SetVariable(p_idx, buf, 0, 0);
-			minim->FixVariable(p_idx);
-		}
+		else
+			minim->SetFixedVariable(p_idx, buf, 0);
+		
 		p_idx++;
 	}
 	for(int i=0;i<e_slices;++i){
@@ -274,12 +267,11 @@ void minimizerParInit(int site, ROOT::Math::Minimizer *minim){
 
 		sprintf(buf, "eps_b12_%d", i);
 		sprintf(par_names[p_idx], "%s", buf);
-		if(withB12){
+		if(withB12)
 			minim->SetLimitedVariable(p_idx, buf, eps_init, eps_init * step_ratio, 0, 1);
-		}else{
-			minim->SetVariable(p_idx, buf, 0, 0);
-			minim->FixVariable(p_idx);
-		}
+		else
+			minim->SetFixedVariable(p_idx, buf, 0);
+		
 		p_idx++;
 	}
 
@@ -295,101 +287,14 @@ void minimizerParInit(int site, ROOT::Math::Minimizer *minim){
 	
 }
 
-
-
-void fitterParInit(int site, TFitter &minuit){
-/*
-		n_range //muon rates  
-		n_range //n_lihe
-		n_range //n_b12
-		e_slices //dc effs
-		e_slices //lihe effs
-		e_slices //b12 effs
-		1 //n_tot
-		1 //lihe ratio
-*/
-
-	double step_ratio = 1e-2;
-	double mu_rate_init = 1e-4;
-	double n_lihe_init = 1e3;
-	double n_b12_init = 1e1;
-	double eps_init = 1.0 / e_slices;
-	double r_lihe_init = 0.5;	
-	char buf[255];
-	int p_idx = 0;
-
-	for(int i=0;i<n_range;++i){
-
-		h[i][0]->Fit("expo","LQN0","", 300, 5000);
-		mu_rate_init = fabs(expo->GetParameter(1));
-		sprintf(buf, "mu_rate_%d", i);
-		sprintf(par_names[p_idx], "%s", buf);
-		minuit.SetParameter(p_idx, buf, mu_rate_init, mu_rate_init * step_ratio, 0, 0);
-		p_idx++;
-
-		sprintf(buf, "n_lihe_%d", i);
-		sprintf(par_names[p_idx], "%s", buf);
-		minuit.SetParameter(p_idx, buf, n_lihe_init, n_lihe_init * step_ratio, 0, 0);
-		p_idx++;
-
-		sprintf(buf, "n_b12_%d", i);
-		sprintf(par_names[p_idx], "%s", buf);
-		if(withB12){
-			minuit.SetParameter(p_idx, buf, n_b12_init, n_b12_init * step_ratio, 0, 0);
-		}else{
-			minuit.SetParameter(p_idx, buf, 0, 0, 0, 0);
-			minuit.FixParameter(p_idx);
-		}
-		p_idx++;
-	}
-	for(int i=0;i<e_slices;++i){
-		eps_init = h[0][i]->GetEntries() / h[0][0]->GetEntries();
-		sprintf(buf, "eps_dc_%d", i);
-		sprintf(par_names[p_idx], "%s", buf);
-		minuit.SetParameter(p_idx, buf, eps_init, eps_init * step_ratio, 0, 1);
-		p_idx++;
-
-		sprintf(buf, "eps_lihe_%d", i);
-		sprintf(par_names[p_idx], "%s", buf);
-		minuit.SetParameter(p_idx, buf, eps_init, eps_init * step_ratio, 0, 1);
-		p_idx++;
-
-		sprintf(buf, "eps_b12_%d", i);
-		sprintf(par_names[p_idx], "%s", buf);
-		if(withB12){
-			minuit.SetParameter(p_idx, buf, eps_init, eps_init * step_ratio, 0, 1);
-		}else{
-			minuit.SetParameter(p_idx, buf, 0, 0, 0, 0);
-			minuit.FixParameter(p_idx);
-		}
-		p_idx++;
-	}
-	for(int i=0;i<n_slice;++i){
-		sprintf(buf,"r_lihe_%d",i);
-		sprintf(par_names[p_idx], "%s", buf);
-		minuit.SetParameter(p_idx, buf, r_lihe_init, r_lihe_init * step_ratio, 0.3, 1);
-		p_idx++;
-	}
-
-	double n_dc_init = h[0][0]->GetEntries();
-	sprintf(par_names[p_idx], "n_dc");
-	minuit.SetParameter(p_idx, "n_dc", n_dc_init, n_dc_init * step_ratio, 0.7 * n_dc_init, 1.3 * n_dc_init);
-		
-	p_idx++;
-	
-}
-
-
 void combinedFit_energy(){
 
-
-	gStyle->SetOptFit(1);
 	char _f_sum[512];
 	for(int i=0;i<n_pdfs;++i)
 		if(i==0)
-			sprintf(_f_sum,"%s",_pdfs[n_pdfs - 1 - i]);
+			sprintf(_f_sum,"%s",_pdfs[i]);
 		else
-			sprintf(_f_sum,"%s+%s",_f_sum,_pdfs[n_pdfs - 1 - i]);
+			sprintf(_f_sum,"%s+%s",_f_sum,_pdfs[i]);
 
 	cout << "INITIALIZING FUNCTION WRAPPERS" << endl;
 	char buf[255];	
@@ -406,30 +311,15 @@ void combinedFit_energy(){
 	ROOT::Fit::DataOptions opt;
 	opt.fUseEmpty = true;
 	
-	//ROOT::Fit::Fitter minuit2;
-	TFitter minuit(npars);	
-	//initialize_fitter(minuit2);
-	initialize_fitter(minuit);
 	for(int site=0;site<3;++site){
-		do_fit(site, minuit, opt, results);
+		do_fit(site, opt, results);
 	}
 	//printLatexTable(results);
 	printResults(results);
 
 }
 
-void initialize_fitter(TFitter &minuit){
-	cout << "INITIALIZING MINIMIZER" << endl;
-	
-	double p0 = 1;
-	TMinuit *_minuit = minuit.GetMinuit();
-	_minuit->SetPrintLevel(1);
-	_minuit->SetErrorDef(0.5);
-	p0 = 2;
-	minuit.ExecuteCommand("SET STRATEGY",&p0,1);
-}
-
-void do_fit(int site, TFitter &minuit, ROOT::Fit::DataOptions &opt, double results[3][npars][2]){
+void do_fit(int site, ROOT::Fit::DataOptions &opt, double results[3][npars][2]){
 	char buf[255];
 	ROOT::Fit::BinData *data[n_range][n_slice];
 	ROOT::Fit::DataRange rangeD(fitMin, fitMax);
@@ -446,35 +336,65 @@ void do_fit(int site, TFitter &minuit, ROOT::Fit::DataOptions &opt, double resul
 			fPNLL[r][s] = new ROOT::Fit::PoissonLikelihoodFCN<ROOT::Math::IBaseFunctionMultiDim>(*data[r][s], *wf[r][s]);
 		}
 	}
+
 	ROOT::Math::Minimizer *minim = ROOT::Math::Factory::CreateMinimizer("Minuit2");
 	ROOT::Math::Functor _func(_likelihood, npars);
 	minim->SetFunction(_func);
-	minim->SetPrintLevel(2);
-	minim->SetStrategy(2);
-	minim->SetMaxFunctionCalls(100000);
-	minimizerParInit(site, minim);
+	initialize_minimizer(site, minim);
 	minim->Hesse();
 	minim->Minimize();
+	minim->Hesse();
 	minim->PrintResults();
-/*
-	fitterParInit(site, minuit);
-	minuit.SetFCN(wrap);
-	minuit.ExecuteCommand("SIMPLEX", simplex_args, 2);
-	minuit.ExecuteCommand("MIGRAD", migrad_args, 2);
-	minuit.ExecuteCommand("HESSE", 0, 0);
-//	minuit.ExecuteCommand("MINOS", minos_args, 1);
-//	minuit.ExecuteCommand("HESSE", 0, 0);
-	//getchar();		
-	double _pars[npars];
-	for(int _i=0;_i<npars;++_i){
-		_pars[_i] = minuit.GetParameter(_i);
-		results[site][_i][0] = minuit.GetParameter(_i);
-		results[site][_i][1] = minuit.GetParError(_i);
+
+	TH2D *h_corr = new TH2D("h_corr","Correlations",npars, 0, npars, npars, 0, npars);
+	for(int i=0;i<npars;++i)
+		for(int j=0;j<npars;++j)
+			h_corr->SetBinContent(i,j,minim->Correlation(i,j));
+	h_corr->Draw("colz");
+	gPad->SetLogx(0);
+	gPad->SetLogy(0);
+	sprintf(buf,"./plots/cfits_energy/corr%d.png",site+1);
+	c1->SaveAs(buf);
+
+	plotHists(site, h, func, minim->X());
+	plotEnergy(site, minim->X(), minim->Errors());
+
+	double *_x = minim->X();
+	double *_x_err = minim->Errors();
+	for(int i=0;i<npars;++i){
+		results[site][i][0] = _x[i];
+		results[site][i][1] = _x_err[i];
 	}
-
-
-	plotHists(site, h, func, _pars);
-*/	
 }
 
+void plotEnergy(int site, double *par, double *par_err){
+	double x[e_slices], x_err[e_slices], y[3][e_slices], y_err[3][e_slices];
 
+	int offset = 3 * n_range;
+	for(int i=0;i<e_slices;++i){
+		x[i] = 0.7 + (12-0.7)/e_slices * (i+1);
+		x_err[i] = (12-0.7) / ( 2 * e_slices );
+		y[0][i] = par[offset + 3*i];
+		y_err[0][i] = par_err[offset + 3*i];
+		y[1][i] = par[offset + 3*i+1];
+		y_err[1][i] = par_err[offset + 3*i+1];
+		y[2][i] = par[offset + 3*i+2];
+		y_err[2][i] = par_err[offset + 3*i+2];
+	}
+	TMultiGraph *mg = new TMultiGraph();
+	TGraph *gr[3];
+	for(int i=0;i<2;++i){
+		gr[i] = new TGraphErrors(e_slices, x, y[i], x_err, y_err[i]);
+		gr[i]->SetMarkerStyle(20);
+		gr[i]->SetMarkerColor(i+2);
+		mg->Add(gr[i]);
+	}
+	mg->SetMinimum(0);
+	mg->Draw("APL");
+	char buf[255];
+	char *dir = "./plots/cfits_energy";
+	sprintf(buf,"%s/cfit_e_%d.png",dir,site+1);
+	gPad->SetLogx(0);
+	gPad->SetLogy(0);
+	c1->SaveAs(buf);
+}
